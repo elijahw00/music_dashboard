5686427d1f236cd9c8dbd85aa951d6ab3953191e
elijahw00 <120138050+elijahw00@users.noreply.github.com>
2025-04-11 06:39:13 +0000

    Enable Spotify login and music recs

--- music_mood_app.py
+++ music_mood_app.py
@@ -1,6 +1,19 @@
+import os
 import streamlit as st
 from textblob import TextBlob
-from spotify_util import get_songs_by_mood
+from spotify_util import get_songs_by_mood, get_current_user
+
+st.title("Music Mood DashBoard")
+st.write("Tell ur how you're feeling, and we''ll suggest some songs!")
+#try getting user info
+try:
+    user = get_current_user()
+    st.success(f"Logged in as {user['display_name']}")
+except:
+    st.warning("Please login to Spotify using the button below")
+    login_url = auth_manager.get_authorize_url()
+    st.markdown(f"[Login to Spotify]({login_user})")
+
 
 #function to analyze the mood of user based on text input
 def analyze_mood(text):
@@ -12,10 +25,7 @@ def analyze_mood(text):
         return 'angry'
     else:
         return 'neutral'
-
-# Set up the Streamlit app
-st.title("Music Mood Dashboard")
-st.write("Tell us how you're feeling, and we'll suggest some songs!")
+    
 # Create a text input for the user to describe their mood
 text_input = st.text_area("How are you feeling today?")
 
@@ -24,13 +34,20 @@ if st.button("Get Music Suggestions"):
     if text_input:
         mood = analyze_mood(text_input)
         st.write(f"So, you're feeling {mood}. Let's find some music for you!")
-        # Get songs based on the analyzed mood
-        songs = get_songs_by_mood(mood)
-        st.subheader("Here are some song suggestions since you're feeling " + mood + ":")
-        # Display the songs
-        for song in songs:
-            st.write(f"{song['name']} by {song['artist']}")
-            st.image(song['image'], width=100)
-            st.markdown(f"[Listen here]({song['url']})")
-    else:
-        st.warning("Please tell us how you're feeling!")
+        try:
+
+            # Get songs based on the analyzed mood
+            songs = get_songs_by_mood(mood)
+            st.subheader("Here are some song suggestions since you're feeling " + mood + ":")
+            # Display the songs
+            for song in songs:
+                st.write(f"{song['name']} by {song['artist']}")
+                st.image(song['image'], width=100)
+                st.markdown(f"[Listen here]({song['url']})")
+            else:
+                st.warning("Please tell us how you're feeling!")
+        except Exception as e:
+            st.error("Something went wrong while fetching songs.")
+            st.exception(e)
+else:
+    st.warning("Please tell us how you're feeling!")
